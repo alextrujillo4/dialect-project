cc1cc93959717dacb60cb4e6907f73afe8105576
alextrujillo <alexandro4v@gmail.com>
2020-03-18 17:24:24 -0600

    credentials added

---
+++ functions/firebase/credentials.js
@@ -0,0 +1,14 @@
+//=== Firebase Database Link ===================================================
+const firebase = require("firebase-admin");
+const serviceAccount = require("../firebase-key-dialect.json");
+firebase.initializeApp({
+    apiKey: "AIzaSyAoa44KA10-7GxuSoKNWDrnStcBBa0Ru7w",
+    authDomain: "dialectbotreclutamiento-shginm.firebaseapp.com",
+    databaseURL: "https://dialectbotreclutamiento-shginm.firebaseio.com",
+    projectId: "dialectbotreclutamiento-shginm",
+    storageBucket: "dialectbotreclutamiento-shginm.appspot.com",
+    messagingSenderId: "691851747789",
+    appId: "1:691851747789:web:39b2aef8c42d2fb4b4e484",
+    credential: firebase.credential.cert(serviceAccount),
+});
+module.exports = firebase
