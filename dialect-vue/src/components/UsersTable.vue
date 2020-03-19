fb7a3cd8e3f72f13f23d0334694be40947a38942
alextrujillo <alexandro4v@gmail.com>
2020-03-18 20:03:07 -0600

    Checkpoint 3 error UsersTable.vue

--- dialect-vue/src/components/UsersTable.vue
+++ dialect-vue/src/components/UsersTable.vue
@@ -72,17 +72,14 @@ export default {
       ],
     };
   },
+  firebase: {
+    users: usersRef,
+  },
   computed: {
     users_data() {
-      return usersRef.once('value', (snapshot) => {
-        const user = snapshot.val();
-        console.log(user);
-        const miMapa = new Map();
-        miMapa.set('user', user);
-        miMapa.set('date_of_interview', moment(user.date_of_interview).format('dddd, D MMMM YYYY - hh:mm a'));
-        miMapa.set('last_interaction', moment(user.last_interaction).format('dddd, D MMMM YYYY'));
-        return miMapa;
-      });
+      return this.users.map((user) => ({
+        ...user, date_of_interview: moment(user.date_of_interview).format('dddd, D MMMM YYYY - hh:mm a'), last_interaction: moment(user.last_interaction).format('dddd, D MMMM YYYY'),
+      }));
     },
   },
 
